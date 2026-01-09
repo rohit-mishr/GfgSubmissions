2c6427637da295817779473d13bd81a9d0615751
Rohit Mishra <130590624+rohit-mishr@users.noreply.github.com>
2026-01-09 19:16:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Row with max 1s/row-with-max-1s.cpp
+++ Difficulty: Medium/Row with max 1s/row-with-max-1s.cpp
@@ -5,26 +5,24 @@ class Solution {
         // code here
         int r = arr.size();
         int c = arr[0].size();
-        int index;
+        int index = -1;
         int max_num = 0;
         for(int i = 0 ; i < r ; i++){
             int low = 0;
             int high = c-1;
-            if(arr[i][c-1]==0){
-                low = c;
-            }
-            else
-            while(low<high){
+            int  temp = c;
+            while(low<=high){
                 int mid = (low+high)/2;
                 //cout<<low<<" "<<mid<<" "<<high<<endl;
                 if(arr[i][mid]==1){
-                    high = mid;
+                    temp = mid;
+                    high = mid-1;
                 }
                 else{
                     low = mid + 1;
                 }
             }
-            int m = c - low;
+            int m = c - temp;
             if(max_num<m){
                 max_num = m;
                 index = i;
